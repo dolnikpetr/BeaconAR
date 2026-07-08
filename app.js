88a110a59753ed875b691fc9f344205443e3fe98
dolnikpetr <dolnik.petr@gmail.com>
2026-07-08 14:37:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./sw.js")
-        .then(() => console.log("Service Worker registered"))
-        .catch(console.error);
+
+    navigator.serviceWorker.register("./sw.js");
+
 }
 
 const API =
     "https://script.google.com/macros/s/AKfycbz3I2onzrPLR7Bcfb-6cbeRtA74hf6utX0YGkIpCV_VKGR4jOPhBhdzzcKatojh6PvZWA/exec";
 
-const camera =
-    document.getElementById("camera");
+const screenLoading =
+    document.getElementById("screenLoading");
 
-const status =
-    document.getElementById("status");
+const screenSelect =
+    document.getElementById("screenSelect");
+
+const scenarioList =
+    document.getElementById("scenarioList");
 
 init();
 
 async function init() {
-
-    status.textContent = "Načítám scénáře...";
 
     await loadScenarios();
 
@@ -25,35 +26,47 @@ async function init() {
 
 async function loadScenarios() {
 
-    try {
+    const response =
+        await fetch(`${API}?action=list`, {
 
-        const url = `${API}?action=list`;
-
-        console.log("Calling:", url);
-
-        const response = await fetch(url, {
-            method: "GET",
-            mode: "cors",
-            redirect: "follow",
             cache: "no-store"
+
         });
 
-        console.log(response);
+    const data =
+        await response.json();
 
-        const data = await response.json();
+    showScenarioSelector(data.scenarios);
 
-        console.log(data);
+}
 
-        status.textContent = "Hotovo";
+function showScenarioSelector(scenarios) {
 
-    }
-    catch (error) {
+    screenLoading.hidden = true;
 
-        console.error(error);
+    screenSelect.hidden = false;
 
-        status.textContent =
-            error.name + ": " + error.message;
+    scenarioList.innerHTML = "";
 
-    }
+    scenarios.forEach(scenario => {
+
+        const button =
+            document.createElement("button");
+
+        button.textContent =
+            scenario.name;
+
+        button.onclick = () => {
+
+            alert(
+                "Vybraný scénář: " +
+                scenario.id
+            );
+
+        };
+
+        scenarioList.appendChild(button);
+
+    });
 
 }
