1271f7c2e7b8e90147d382eef8319e2bfd5fe342
dolnikpetr <dolnik.petr@gmail.com>
2026-07-08 13:06:02 +0200

    Update app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+const camera =
+    document.getElementById("camera")
+
+const status =
+    document.getElementById("status")
+
+init()
+
+async function init() {
+
+    status.textContent =
+        "Žádám o přístup ke kameře..."
+
+    try {
+
+        const stream =
+            await navigator.mediaDevices.getUserMedia({
+
+                video: {
+
+                    facingMode: {
+
+                        ideal: "environment"
+
+                    }
+
+                },
+
+                audio: false
+
+            })
+
+        camera.srcObject = stream
+
+        await camera.play()
+
+        status.textContent =
+            "✅ Kamera běží"
+
+    }
+
+    catch (error) {
+
+        console.error(error)
+
+        status.textContent =
+            error.name + ": " + error.message
+
+    }
+
+}
