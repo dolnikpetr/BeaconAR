3ed760b0ed20277b5636d1db9051705b6d765b0f
dolnikpetr <dolnik.petr@gmail.com>
2026-07-08 13:40:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
-
     navigator.serviceWorker.register("./sw.js")
-
+        .then(() => console.log("Service Worker registered"))
+        .catch(console.error);
 }
 
 const camera =
